4251fa96404895042f5bd4f438569c64ae12021a
avpm <angelomartins90@gmail.com>
2022-07-26 18:06:21 -0300

    :construction: adding images

--- src/components/HomeUser/ViewOrders/index.js
+++ src/components/HomeUser/ViewOrders/index.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { api } from "../../../api/api";
-import { useNavigate, Link } from "react-router-dom";
 import { Card } from "antd";
 
 export function ViewOrders() {
