c1eeb27124564de274e7ddb105fd07fcfb3c89f4
Tathyanna Maximiano <tathyanna.maximiano@gmail.com>
2022-07-27 13:23:16 -0400

    :lipstick:store style initializing

--- src/pages/Store/index.js
+++ src/pages/Store/index.js
@@ -2,20 +2,31 @@ import { TripCard } from '../../components/Store/TripCard';
 import { Cart } from '../../components/Store/Cart';
 import { CartProvider } from 'react-use-cart';
 import { NavBar } from '../../components/HomePage/navBar';
+import { ShoppingCartOutlined } from '@ant-design/icons';
+import style from '../Store/style.module.css';
 
 export function Store() {
 	return (
 		<>
 			<NavBar />
 			<div
-				className="container"
+				className={style.container}
 				style={{ display: 'flex', flexDirection: 'row' }}
 			>
 				<CartProvider>
-					<div className="tripCard" style={{ width: '70vw', height: '70vh' }}>
+					<div className={style.tripCard}>
 						<TripCard />
 					</div>
-					<div className="cart">
+					<div
+						className={style.cart}
+						style={{
+							width: '22vw',
+							height: '85vh',
+						}}
+					>
+						<h1>
+							Cart <ShoppingCartOutlined />
+						</h1>
 						<Cart />
 					</div>
 				</CartProvider>
