592c154e6e7c59a7f01d7c5de5456ab6e34e7bae
Tulio Minini <tulio.mminini@gmail.com>
2022-07-28 13:24:09 -0300

    herokuapp added

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiURLs = {
   development: "http://localhost:4000",
-  production: "https://project-three-ironhack.netlify.app",
+  production: "https://project-three-back-end.herokuapp.com",
 };
 
 const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
