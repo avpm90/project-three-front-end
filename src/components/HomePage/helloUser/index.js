0340c4a95e04cd36e0f5355ae970b2e7fbd513c0
Tulio Minini <tulio.mminini@gmail.com>
2022-07-27 23:13:54 -0300

    :lipstick: homePage style v1

--- src/components/HomePage/helloUser/index.js
+++ src/components/HomePage/helloUser/index.js
@@ -11,7 +11,8 @@ export function Hello() {
     <div className={style.helloContainer}>
       {/* {loggedInUser ? <p>{`Hello, ${loggedInUser.user.name}`}</p> : null} */}
       <>
-        <h1 className={style.helloH1}>Where do You Wanna Go?</h1>
+        <h1 className={style.helloH1}>Where do You</h1>
+        <h1 className={style.helloH1b}>Wanna Go?</h1>
         <h3 className={style.helloH3}>
           We have a selection of nice and surprising destinations to take you
         </h3>
